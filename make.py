23c1759731afb159d1bb00e3be2936cc8eb1c1ea
Jan Hohmann <github@jnhmn.de>
2016-11-16 21:07:52 +0100

    Convert input parameters to mm

--- make.py
+++ make.py
@@ -3,79 +3,96 @@
 from subprocess import call
 from subprocess import Popen
 from time import sleep
-import quantities
-print("Enter Filenmae")
-file_name = input()
+
+MM2IN = 0.039370079
+MM2BP = 2.834645688
+
+def BPs (ival):
+  return str(ival*MM2BP)
+
+file_name = input("Enter filename: ")
 
 Popen(['pdfseparate', '-f', '2', file_name, 'part_%d.pdf'])
 
 print("Tuning left Border")
 print("Decimal mark is \".\"")
 
-tmp_left = "18.65"
-tmp_bottom = "30.15"
-tmp_right = "573.1"
-tmp_top = "811.3"
-left = "1"
-bottom = "1"
-right = "50"
-top = "50"
+tmp_left = 6.5
+tmp_bottom = 10.5
+tmp_right = 202
+tmp_top = 286
+left = 1
+bottom = 1
+right = 50
+top = 50
 
-print("Example Values are 18 30 575 815")
+print("Example Values are 6.5 10.5 202 286")
 
 
-while tmp_left != '':
+while tmp_left != -1.0:
   left = tmp_left
-  Popen(["pdfcrop", "--bbox", left+" "+bottom+" "+right+" "+top, "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
+  Popen(["pdfcrop", "--bbox", BPs(left)+" "+BPs(bottom)+" "+BPs(right)+" "+BPs(top), "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
   sleep(0.5)
   print()
-  tmp_left = input("Enter left margin: ")
+  try:
+    tmp_left = float(input("Enter left margin: "))
+  except ValueError:
+    tmp_left = -1.0
 
-while tmp_bottom != '':
+while tmp_bottom != -1.0:
   bottom = tmp_bottom
-  Popen(["pdfcrop", "--bbox", left+" "+bottom+" "+right+" "+top, "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
+  Popen(["pdfcrop", "--bbox", BPs(left)+" "+BPs(bottom)+" "+BPs(right)+" "+BPs(top), "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
   sleep(0.5)
   print()
-  tmp_bottom = input("Enter bottom margin: ")
+  try:
+    tmp_bottom = float(input("Enter bottom margin: "))
+  except ValueError:
+    tmp_bottom = -1.0
 
-while tmp_right != '':
+while tmp_right != -1.0:
   right = tmp_right
-  Popen(["pdfcrop", "--bbox", left+" "+bottom+" "+right+" "+top, "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
+  Popen(["pdfcrop", "--bbox", BPs(left)+" "+BPs(bottom)+" "+BPs(right)+" "+BPs(top), "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
   sleep(0.5)
   print()
-  tmp_right = input("Enter right position: ")
+  try:
+    tmp_right = float(input("Enter right position: "))
+  except ValueError:
+    tmp_right = -1.0
 
-while tmp_top != '':
+while tmp_top != -1.0:
   top = tmp_top
-  Popen(["pdfcrop", "--bbox", left+" "+bottom+" "+right+" "+top, "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
+  Popen(["pdfcrop", "--bbox", BPs(left)+" "+BPs(bottom)+" "+BPs(right)+" "+BPs(top), "--margins", "0", "part_2.pdf", "cropped_2.pdf"])
   sleep(0.5)
   print()
-  tmp_top = input("Enter top position: ")
+  try:
+    tmp_top = float(input("Enter top position: "))
+  except ValueError:
+    tmp_top = -1.0
 
-rows = input("Please enter number of rows: ")
-cols = input("Please enter number of columns: ")
+rows = int(input("Please enter number of rows: "))
+cols = int(input("Please enter number of columns: "))
 
 file_names = []
 num_files = int(rows)*int(cols)
-start = int(rows) + 1
+start = rows + 1
 while start >= 2:
   colcnt = 0
   offset = 0
-  while colcnt < int(cols):
+  while colcnt < cols:
     file_id = start+offset
     tmp_file_name = "part_"+str(file_id)+".pdf"
     file_name = "cropped_"+str(file_id)+".pdf"
-    Popen(["pdfcrop", "--bbox", left+" "+bottom+" "+right+" "+top, "--margins", "0", tmp_file_name, file_name])
+    Popen(["pdfcrop", "--bbox", BPs(left)+" "+BPs(bottom)+" "+BPs(right)+" "+BPs(top), "--margins", "0", tmp_file_name, file_name])
     file_names.append(file_name)
-    offset += int(rows)
+    offset += rows
     colcnt += 1
   start -= 1
 print(file_names)
 print("Sleeping for 10 secons")
 sleep(10)
 
-sizex = (float(right) - float(left)) * float(cols)
-sizey = (float(top) - float(bottom)) * float(rows)
+sizex = BPs((right - left) * cols)
+sizey = BPs((top - bottom) * rows)
 
-command = ['pdfnup', '--nup', str(cols)+'x'+str(rows), '--papersize', '{'+str(sizey)+'bp, '+str(sizex)+'bp}', '--outfile', 'comb.pdf']
+command = ['pdfnup', '--nup', str(cols)+'x'+str(rows), '--papersize', '{'+sizey+'bp, '+sizex+'bp}', '--outfile', 'comb.pdf']
 Popen(command + file_names)
